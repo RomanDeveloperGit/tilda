23cc25009e350248cc71e33f0196dfd41c94c641
roman_orlov <roman.developer@mail.ru>
2022-03-23 14:45:54 +0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 const elements = {
 	loading: document.querySelector( ".product-page__loading" ),
 	productsBasketCount: document.querySelector( ".product-page__basket-counter" ),
-	productItem: document.querySelector( ".product-item" )
+	productItem: document.querySelector( ".product-item" ),
+	productImageBox: document.querySelector( ".product-item__img-box" ),
+	productTitle: document.querySelector( ".product-item__title" ),
+	productDescription: document.querySelector( ".product-item__description" ),
+	productCurrentPrice: document.querySelector( ".product-item__current-price" ),
+	productOldPrice: document.querySelector( ".product-item__old-price" ),
+	productQuantityCount: document.querySelector( ".product-item__quantity-count" ),
+	productBasketButton: document.querySelector( ".product-item__basket-button" )
 };
 
 const API_URL = "https://store.tildacdn.com/api/tgetproduct/";
@@ -17,22 +24,6 @@ const productData = {
 	currentImageIndex: 0
 };
 
-const updateProductsBasket = newCount => {
-	productsBasketCount = newCount;
-
-	elements.productsBasketCount.innerHTML = productsBasketCount;
-};
-
-const updateProductData = ( title, description, currentPrice, oldPrice, quantityCount, images ) => {
-	productData.title = title;
-	productData.description = description;
-	productData.currentPrice = currentPrice;
-	productData.oldPrice = oldPrice;
-	productData.quantityCount = quantityCount;
-	productData.images = images.map( obj => obj.img );
-	productData.currentImageIndex = 0;
-};
-
 const setLoading = isActive => {
 	if (isActive) {
 		elements.loading.style.display = "block";
@@ -44,31 +35,50 @@ const setLoading = isActive => {
 	}
 };
 
-const initProductPage = () => {
-	setLoading( true );
+const setProductsBasket = newCount => {
+	productsBasketCount = newCount;
 
-	setTimeout( () => setLoading( false ), 1000 );
-	updateProductsBasket( 0 );
-	
-	updateProductData(
-		"Studio Headphones",
-		"When we first checked out our new headphones, we noticed the box said 'improved bass by cool. We had to wonder, is this marketing jargon, or the real thing? But it only took a moment to realize that bass was not kidding.",
-		1195,
-		1940,
-		10,
-		[
-			{
-				img: "https://static.tildacdn.com/tild3235-6561-4364-a534-623132376536/HDTwo_AngleUp_OnGray.jpeg"
-			},
-			{
-				img: "https://static.tildacdn.com/tild3036-3464-4130-b033-376664633332/HDTwo_Rotated_OnGray.jpeg"
-			}
-		]
-	);
+	elements.productsBasketCount.innerHTML = productsBasketCount;
 };
 
-console.log( productData );
+const setCurrentImageIndex = newIndex => {
+	const maxImageIndex = productData.images.length - 1;
+
+	if (newIndex < 0) newIndex = maxImageIndex;
+	else if (newIndex > maxImageIndex) newIndex = 0;
+
+	productData.currentImageIndex = newIndex;
+};
+
+const setProductData = ( title, description, currentPrice, oldPrice, quantityCount, images ) => {
+	productData.title = title;
+	productData.description = description;
+	productData.currentPrice = currentPrice;
+	productData.oldPrice = oldPrice;
+	productData.quantityCount = quantityCount;
+	productData.images = images.map( obj => obj.img );
+
+	// Если осталось 0 товара, то кнопку в disabled бросаем.
+
+	setCurrentImageIndex( 0 );
+};
+
+const initProductPage = async() => {
+	setLoading( true );
+
+	try {
+		const response = await fetch( API_URL );
+		const data = await response.json();
+
+		setProductsBasket( 0 );
+		setProductData( data.title, data.descr, data.price, data.oldprice, data.quantity, JSON.parse( data.images ) );
+	}
+	catch (error) {
+		console.error( error );
+	}
+	finally {
+		setLoading( false );
+	}
+};
 
 initProductPage();
-
-console.log( productData );
